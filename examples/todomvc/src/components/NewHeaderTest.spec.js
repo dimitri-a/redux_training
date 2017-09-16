f8632955d3c010df5d61307409ef744ee4eca871
dimitric <couwenberg@gmail.com>
2017-09-16 17:04:44 +1000

    unit test with testing method with param

--- examples/todomvc/src/components/NewHeaderTest.spec.js
+++ examples/todomvc/src/components/NewHeaderTest.spec.js
@@ -22,6 +22,9 @@ describe('Header enzyme style', () => {
 
     output.props.children.props.onSave('');
     expect(props.addTodo).not.toBeCalled()
+
+    output.props.children.props.onSave('fsakhfkjsdh');
+    expect(props.addTodo).toBeCalled()
     //expect(output.props.children.props.onSave).toBe('div');
 
 
@@ -29,15 +32,25 @@ describe('Header enzyme style', () => {
 
 
 
+  it('should call addTodo if length of text is greater than 0', () => {
 
+    const props = {
+      addTodo: jest.fn()
+    }
 
-  xit('should call addTodo if length of text is greater than 0', () => {
-    // const { output, props } = setup()
-    // const input = output.props.children[1]
-    // input.props.onSave('')
-    // expect(props.addTodo).not.toBeCalled()
-    // input.props.onSave('Use Redux')
-    // expect(props.addTodo).toBeCalled()
+    const renderer = createRenderer();
+    renderer.render(<Header {...props} />)
+    const output = renderer.getRenderOutput()
+    console.log('output=',output.props.children);;
+
+    //todo expect component top level type
+    expect(output.type).toBe('header');
+
+    output.props.children.props.onSave('');
+    expect(props.addTodo).not.toBeCalled()
+
+    output.props.children.props.onSave('fsakhfkjsdh');
+    expect(props.addTodo).toBeCalled()
   })
 
 
